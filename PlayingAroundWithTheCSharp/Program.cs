100d0ad077ba15fabf6d5943ec3367d72ea520af
Octavio Avila <oavil004@fiu.edu>
2020-03-31 20:57:25 -0700

    Add project files.

---
+++ PlayingAroundWithTheCSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PlayingAroundWithTheCSharp
+{
+    class Program
+    {
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, there Octavio. Welcome back!");
+
+            Program program = new Program();
+
+            int sum = program.Add(1, 2);
+
+            Console.WriteLine("The sum of 1 and 2 is " + sum);
+        }
+    }
+}
